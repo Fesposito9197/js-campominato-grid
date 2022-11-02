028e441e3d8064c63222327f2054f4f3f4423794
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-02 17:00:05 +0100

    refactorig click event

--- js/main.js
+++ js/main.js
@@ -21,60 +21,7 @@ boardContainer.innerHTML = "";
 play.addEventListener("click" , function() {
     
     renderBoard(level.value)
-    // if(level.value === "100"){
-        
-    //     renderBoard(maxNumber)
-    //     console.log( level.value )
-    //     console.log( maxNumber )
-        
-    // }else if (level.value === "81"){
-        
-    //     renderBoard(maxNumber)
-    //     console.log( level.value )
-    //     console.log( maxNumber )
-    // }else{
-        
-    //     renderBoard(maxNumber)
-    //     console.log( level.value )
-    //     console.log( maxNumber )
-    // }
-    //     if (level.value === "easy") {
-    //         for (let i = 1; i <= 100; i++) {
-    //             const boardCell = document.createElement("div");
-    //             boardCell.innerHTML = i;
-    //             boardCell.classList.add("board-number");
-    //             boardCell.addEventListener("click" , function() {
-    //                 this.classList.add("number-selected");
-    //                 console.log(this.innerHTML);
-    //             })
-    //             boardContainer.append(boardCell);
-                
-    //         }
-    //    } else if ( level.value == "medium"){
-    //         for (let i = 1; i <= 81; i++) {
-    //             const boardCell = document.createElement("div");
-    //             boardCell.innerHTML = i;
-    //             boardCell.classList.add("board-number");
-    //             boardCell.addEventListener("click" , function() {
-    //                 this.classList.add("number-selected");
-    //                 console.log(this.innerHTML);
-    //             })
-    //             boardContainer.append(boardCell);
-                
-    //         }
-    //    } else{
-    //         for (let i = 1; i <= 49; i++) {
-    //             const boardCell = document.createElement("div");
-    //             boardCell.innerHTML = i;
-    //             boardCell.classList.add("board-number");
-    //             boardCell.addEventListener("click" , function() {
-    //                 this.classList.add("number-selected");
-    //                 console.log(this.innerHTML);
-    //             })
-    //             boardContainer.append(boardCell);
-                
-    //         }
-    //    }
+   
     
     // se level e uguale ad easy allora creo un ciclo for che mi crea 100 caselle 
     // se level e uguale a medium allora creo un ciclo fo che mi crea 81 caselle 
